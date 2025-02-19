9785ad0ed7983e8399c34b9f97f2e6d39d76100c
MitryDim <dimcorb@gmail.com>
2025-02-19 09:42:11 +0100

    Ajout de l'importation de console.sol pour faciliter le débogage dans le contrat ResourceToken.

--- hardhat/contracts/RessourceToken.sol
+++ hardhat/contracts/RessourceToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 contract ResourceToken is ERC721URIStorage {
     uint256 private _tokenIds;
